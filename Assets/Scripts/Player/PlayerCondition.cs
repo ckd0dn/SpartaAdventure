a0df2a5b11e2e90cb8eb410e5788bf7c844d4588
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2024-05-27 20:45:31 +0900

    fix: jumpPad, animation
    점프패드 글자 적용
    걷기 애니메이션 이벤트 제거

---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uICondition;
+
+    Condition health { get { return uICondition.health; } }
+    Condition hunger { get { return uICondition.hunger; } }
+    Condition stamina { get { return uICondition.stamina; } }
+
+    public float noHungerHealthDecay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if(hunger.curValue <= 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if(health.curValue <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+
+    public void Die()
+    {
+        Debug.Log("аж╠щ");
+    }
+}
