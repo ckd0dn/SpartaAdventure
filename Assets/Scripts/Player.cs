99882409f298f98e35b6e3fb264e3a0756447c79
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2024-05-27 16:10:43 +0900

    feat: UI
    체력, 배고픔, 스태미나 UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
+    public PlayerCondition condition;
 
     private void Awake()
     {
-        controller = GetComponent<PlayerController>();
         CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
     }
 }
